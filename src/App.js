938a811c1847e4b49c637cbb2215a0adc0987d8e
karkran <nikhil.karkra@anz.com>
2018-06-15 22:40:57 +1000

    render component on click

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
